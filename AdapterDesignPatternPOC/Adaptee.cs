476223778180faa7fa6da3805240e4a84be65b0f
Adithyaav123 <93602832+Adithyaav123@users.noreply.github.com>
2023-10-05 18:36:32 +0530

    Add files via upload

---
+++ AdapterDesignPatternPOC/Adaptee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterDesignPatternPOC
+{
+    internal class Adaptee
+    {
+        public string GetSpecificRequest()
+        {
+            return "Provide the Credentials.";
+        }
+    }
+}
---
+++ AdapterDesignPatternPOC/Adapter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterDesignPatternPOC
+{
+    internal class Adapter : ITarget
+    {
+        private readonly Adaptee adaptee;
+
+        public Adapter(Adaptee adaptee)
+        {
+            this.adaptee = adaptee;
+        }
+        public string GetRequest()
+        {
+            return $"You have to'{this.adaptee.GetSpecificRequest()}'";
+        }
+
+    }
+}
